48fb10efe5cf20d2caf860cc09d8257713216606
David <david_elvir@unitec.edu>
2017-04-28 13:13:43 -0600

    principio del proyecto

---
+++ lab2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+char opcion;
+  bool vivo = true;
+  while(vive){
+    cout<<"Elija el ejercicio: "<<endl
+      cout<<"a. Ejercicio 1"<<endl
+      cout<<"b. Ejercicio 2"<<endl
+      cout<<"c. Salir"
+  }
+
+}
