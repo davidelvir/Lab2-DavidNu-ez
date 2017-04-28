d455ee1732cc51639aee1a01f4681e3c405e2ad4
David <david_elvir@unitec.edu>
2017-04-28 13:37:47 -0600

    ejercicio 1 terminado

--- lab2.cpp
+++ lab2.cpp
@@ -1,15 +1,45 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
+int factorial(int);
+float euler(int);
 
 int main(){
-char opcion;
+int opcion;
   bool vivo = true;
-  while(vive){
+  while(vivo){
     cout<<"Elija el ejercicio: "<<endl
-      cout<<"a. Ejercicio 1"<<endl
-      cout<<"b. Ejercicio 2"<<endl
-      cout<<"c. Salir"
-  }
+    <<"Ejercicio 1: 1"<<endl
+    <<"Ejercicio 2: 2"<<endl
+    <<"Salir: 3"<<endl;
+    cin>>opcion;
+    switch(opcion){
+      case 1:
+        int x;
+        cout<<"Ingrese un entero: ";
+        cin>>x;
+        cout<<euler(x);<<endl;
+        break;
+      case 2:
 
+        break;
+      case 3:
+        vivo = false;
+        break;
+    }
+  }
+}
+int factorial(int x){
+  if(x==0){
+    return 1;
+  }else{
+    return x * factorial(x-1);
+  }
+}
+float euler(int x){
+  float suma=0;
+  for(int i=0;i<13;i++){
+    suma+=((pow(x,i))/factorial(i));
+  }
+  return suma;
 }
